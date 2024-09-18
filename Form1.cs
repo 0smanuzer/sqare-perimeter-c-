5e580ecd646a9e1284b37ab00daa5d774e736bf2
asmr_web <89978742+0smanuzer@users.noreply.github.com>
2024-09-18 16:15:09 +0300

    Update Form1.cs
    feat:оптимизирован код, ускорение работы(optimized code, faster work)

--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp25
@@ -17,56 +10,13 @@ namespace WindowsFormsApp25
             InitializeComponent();
         }
 
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox3_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label4_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox4_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
-            double x1, x2, y1, y2;
-            if (double.TryParse(textBox1.Text, out x1) && double.TryParse(textBox2.Text, out x2) &&
-                double.TryParse(textBox3.Text, out y1) && double.TryParse(textBox4.Text, out y2))
+            if (TryGetDouble(textBox1, out double x1) && TryGetDouble(textBox2, out double x2) &&
+                TryGetDouble(textBox3, out double y1) && TryGetDouble(textBox4, out double y2))
             {
                 double smallerSideLength = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-
                 double largerSideLength = smallerSideLength * 2.5;
-
                 double area = smallerSideLength * largerSideLength;
                 double perimeter = 2 * (smallerSideLength + largerSideLength);
 
@@ -74,9 +24,9 @@ namespace WindowsFormsApp25
             }
         }
 
-        private void textBox5_TextChanged(object sender, EventArgs e)
+        private bool TryGetDouble(TextBox textBox, out double value)
         {
-
+            return double.TryParse(textBox.Text, out value);
         }
     }
 }
